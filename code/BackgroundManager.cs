319263282a14dc3b67808f8b3248a9321c171069
James King <james.king@facepunchstudios.com>
2022-10-16 21:02:54 +0100

    Use a single tiled sprite for the background

--- code/BackgroundManager.cs
+++ code/BackgroundManager.cs
@@ -26,36 +26,13 @@ public class BackgroundManager
 
         _tiles.Clear();
 
+        var boundsSize = MyGame.Current.BOUNDS_MAX - MyGame.Current.BOUNDS_MIN + new Vector2( 8f, 8f );
 
-        var x_min = MyGame.Current.BOUNDS_MIN.x;
-        var y_min = MyGame.Current.BOUNDS_MIN.y;
-        var x_max = MyGame.Current.BOUNDS_MAX.x;
-        var y_max = MyGame.Current.BOUNDS_MAX.y;
-
-        var x = x_min - TILE_WIDTH / 2f - 0.2f;
-        var y = y_min - TILE_HEIGHT / 2f - 0.2f;
-
-        while (x < x_max + TILE_WIDTH)
-        {
-            y = y_min;
-
-            while (y < y_max + TILE_HEIGHT)
-            {
-                AddTile(new Vector2(x, y));
-
-                y += TILE_HEIGHT;
-            }
-
-            x += TILE_WIDTH;
-        }
-    }
-
-    void AddTile(Vector2 pos)
-    {
-        var tile = new BackgroundTile();
-        tile.Position = pos;
-        tile.Scale = new Vector2(TILE_WIDTH, TILE_HEIGHT);
-        tile.Depth = -(220f + _tiles.Count * 0.1f);
-        _tiles.Add(tile);
+		_tiles.Add( new BackgroundTile
+		{
+			Depth = -512f,
+			Scale = boundsSize,
+			UvRect = new Rect( 0f, boundsSize / new Vector2( TILE_WIDTH, TILE_HEIGHT ) )
+		} );
     }
 }
