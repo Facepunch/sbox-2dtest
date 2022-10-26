43aa22eb370dc7762c5c0bf8dea472e24b2192e5
James King <james.king@facepunchstudios.com>
2022-10-26 12:21:43 +0100

    Use a single background sprite again

--- code/BackgroundManager.cs
+++ code/BackgroundManager.cs
@@ -14,10 +14,6 @@ public class BackgroundManager
     public const float TILE_HEIGHT = 3f;
 
     private List<BackgroundTile> _tiles = new List<BackgroundTile>();
-    public BackgroundManager()
-    {
-
-    }
 
     public void Restart()
     {
@@ -28,38 +24,11 @@ public class BackgroundManager
 
         var boundsSize = MyGame.Current.BOUNDS_MAX - MyGame.Current.BOUNDS_MIN + new Vector2( 8f, 8f );
 
-        var x_min = MyGame.Current.BOUNDS_MIN.x;
-        var y_min = MyGame.Current.BOUNDS_MIN.y;
-        var x_max = MyGame.Current.BOUNDS_MAX.x;
-        var y_max = MyGame.Current.BOUNDS_MAX.y;
-
-        var x = x_min - TILE_WIDTH / 2f - 0.2f;
-        var y = y_min - TILE_HEIGHT / 2f - 0.2f;
-
-        while (x < x_max + TILE_WIDTH)
+        _tiles.Add( new BackgroundTile
         {
-            y = y_min;
-
-            while (y < y_max + TILE_HEIGHT)
-            {
-                AddTile(new Vector2(x, y));
-
-                y += TILE_HEIGHT;
-            }
-
-            x += TILE_WIDTH;
-        }
-    }
-
-    void AddTile(Vector2 pos)
-    {
-        var boundsSize = MyGame.Current.BOUNDS_MAX - MyGame.Current.BOUNDS_MIN + new Vector2(8f, 8f);
-
-        _tiles.Add(new BackgroundTile
-        {
-            Depth = -512f,
-            Scale = boundsSize,
-            UvRect = new Rect(0f, boundsSize / new Vector2(TILE_WIDTH, TILE_HEIGHT))
-        });
-    }
+	        Depth = -512f,
+	        Scale = boundsSize,
+	        UvRect = new Rect( 0f, boundsSize / new Vector2( TILE_WIDTH, TILE_HEIGHT ) )
+        } );
+	}
 }
